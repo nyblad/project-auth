2382fd46c1fa79c766700f1a6dd04135ad9129b0
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-18 22:03:03 +0100

    Refactor logic for user login

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
-import styled from 'styled-components/macro'
-import { Button } from 'components/Button'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components/macro';
+import { Button } from 'components/Button';
 
 const Form = styled.form`
   margin: 15px 0;
@@ -8,7 +9,7 @@ const Form = styled.form`
   padding: 20px 20px;
   display: flex;
   flex-direction: column;
-  background: rgba(255,255,255, 0.4);
+  background: rgba(255, 255, 255, 0.4);
   border-radius: 6px;
   @media (min-width: 668px) {
     width: 80%;
@@ -20,16 +21,16 @@ const Form = styled.form`
   @media (min-width: 992px) {
     width: 50%;
   }
-`
+`;
 const Label = styled.label`
   width: 100%;
   padding: 5px 0;
-`
+`;
 const LabelText = styled.p`
   margin: 15px 0 8px 0;
   font-size: 16px;
   font-weight: bold;
-`
+`;
 const Input = styled.input`
   width: 100%;
   padding: 10px;
@@ -37,35 +38,54 @@ const Input = styled.input`
   border-radius: 3px;
   font-size: 16px;
   font-family: 'Open Sans', sans-serif;
-`
+`;
 
 export const LoginForm = () => {
-
   const [formValues, setFormValues] = useState({
     email: '',
-    password: '',
+    password: ''
   });
+  const history = useHistory();
 
-  const clearInputs = () => {
-    setFormValues({
-      email: '',
-      password: '',
+  // const clearInputs = () => {
+  //   setFormValues({
+  //     email: '',
+  //     password: ''
+  //   });
+  // };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.table(formValues);
+    fetch('http://localhost:8080/sessions', {
+      method: 'POST',
+      body: JSON.stringify(formValues),
+      headers: { 'Content-Type': 'application/json' }
     })
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    clearInputs()
-    window.alert(`Now user should be logged in and redirected to secret page.`)
-  }
+      .then(response => response.json())
+      .then(data => {
+        // You'll need to store the access token you get back in the browser using local storage
+        // and then use that token when making other requests to your API
+        console.log(data);
+        localStorage.setItem('accessToken', data.accessToken);
+        setFormValues({
+          name: '',
+          email: '',
+          password: ''
+        });
+      });
+    console.log(`User logged in and redirected to secret page.`);
+    history.push('/secrets');
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
-
       <Label>
         <LabelText>E-mail</LabelText>
         <Input
-          onChange={event => setFormValues({ ...formValues, email: event.target.value })}
+          onChange={event =>
+            setFormValues({ ...formValues, email: event.target.value })
+          }
           value={formValues.email}
           type="email"
           placeholder="mail@mail.com"
@@ -78,15 +98,16 @@ export const LoginForm = () => {
       <Label>
         <LabelText>Password</LabelText>
         <Input
-          onChange={event => setFormValues({ ...formValues, password: event.target.value })}
+          onChange={event =>
+            setFormValues({ ...formValues, password: event.target.value })
+          }
           value={formValues.password}
           type="password"
           placeholder="**********"
           required
         />
       </Label>
-      <Button type="submit" title='Login' />
-
+      <Button type="submit" title="Login" />
     </Form>
-  )
-}
+  );
+};
