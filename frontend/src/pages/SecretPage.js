e5c017de6b685c139d38487521456221ecc7f0ed
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-18 22:36:08 +0100

    Reafactoring and adding accessToken to GET request

--- frontend/src/pages/SecretPage.js
+++ frontend/src/pages/SecretPage.js
@@ -1,31 +1,55 @@
-import React from 'react'
-import styled from 'styled-components/macro'
-import { Button } from 'components/Button'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components/macro';
+import { Button } from 'components/Button';
 
 const Wrapper = styled.main`
   width: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
-  background: #4C5B61;
-`
+  background: #4c5b61;
+`;
 const Text = styled.p`
   font-size: 18px;
   color: #fff;
-`
+`;
 
 export const SecretPage = () => {
+  const [shows, setShows] = useState();
+  const accessToken = localStorage.getItem('accessToken');
 
   const handleLogout = () => {
-    window.alert('Now you should be logged out and redirected to start page')
-  }
+    window.alert('Now you should be logged out and redirected to start page');
+    localStorage.removeItem('accessToken');
+  };
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    fetch('http://localhost:8080/secrets', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `${accessToken}`
+      },
+      signal: abortController.signal
+    })
+      .then(response => response.json())
+      .then(data => {
+        setShows(data.data);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
 
   return (
     <Wrapper>
       <Text>Welcome to the secret page.</Text>
-      <Button onClick={handleLogout} title='Log out' />
+      {accessToken && <Button onClick={handleLogout} title="Log out" />}
     </Wrapper>
-  )
-}
+  );
+};
