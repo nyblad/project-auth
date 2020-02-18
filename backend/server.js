e96ed0a231ba4ee00b1ddbd3f85c1a669e28653e
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-18 21:16:59 +0100

    Add netflix data to authenticated route response

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt-nodejs'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
+import netflixData from './data/netflix-data.json';
 
 // MONGOOSE SETUP
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 // MODEL FOR USER
 const User = mongoose.model('User', {
@@ -31,62 +32,71 @@ const User = mongoose.model('User', {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex')
   }
-})
+});
 
 // MIDDLEWARE TO CHECK ACCESSTOKENS (IF THE USER MATCH ANY ACCESSTOKEN IN DB)
 const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  const user = await User.findOne({ accessToken: req.header('Authorization') });
   if (user) {
-    req.user = user
-    next()
+    req.user = user;
+    next();
   } else {
-    res.status(401).json({ loggedOut: true })
+    res.status(401).json({ loggedOut: true });
   }
-}
+};
 
 // PORT & APP SETUP
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // MIDDLEWARES TO EABLE CORS AND JSON BODY PARSING
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Project 20 @ Technigo: Sofie & Fredrik')
-})
+  res.send('Project 20 @ Technigo: Sofie & Fredrik');
+});
 
 // ROUTE TO REGISTRY A USER
 app.post('/users', async (req, res) => {
   try {
-    const { name, email, password } = req.body
-    const newUser = await new User({ name, email, password: bcrypt.hashSync(password) })
-    newUser.save()
-    res.status(201).json(newUser)
+    const { name, email, password } = req.body;
+    const newUser = await new User({
+      name,
+      email,
+      password: bcrypt.hashSync(password)
+    });
+    newUser.save();
+    res.status(201).json(newUser);
   } catch (err) {
-    res.status(400).json({ messsage: 'Could not create user', error: err.errors })
+    res
+      .status(400)
+      .json({ messsage: 'Could not create user', error: err.errors });
   }
-})
+});
 
 // PROTECTING OUR ROUTE /SECRETS
-app.get('/secrets', authenticateUser)
+// app.get('/secrets', authenticateUser)
 // ROUTE FOR SECRETS
-app.get('/secrets', (req, res) => {
-  res.json({ secret: 'This is a super secret message' })
-})
+app.get('/secrets', authenticateUser, (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: netflixData
+  });
+});
 
 // ROUTE FOR LOGIN - FINDS A USER INSTEAD OF CREATE
 app.post('/sessions', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email })
+  const user = await User.findOne({ email: req.body.email });
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken })
+    res.json({ userId: user._id, accessToken: user.accessToken });
   } else {
-    res.json({ notFound: true })
+    res.json({ notFound: true });
   }
-})
+});
 
 // START THE SERVER
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
