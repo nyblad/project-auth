789277d261cef6b8e29a4818f04c40dd5b571db1
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2020-02-18 21:36:39 +0100

    Moving setFormValues to fetch callback function

--- frontend/src/components/RegistryForm.js
+++ frontend/src/components/RegistryForm.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import styled from 'styled-components/macro'
-import { Button } from 'components/Button'
+import React, { useState } from 'react';
+import styled from 'styled-components/macro';
+import { Button } from 'components/Button';
 
 const Form = styled.form`
   margin: 15px 0;
@@ -8,7 +8,7 @@ const Form = styled.form`
   padding: 20px 20px;
   display: flex;
   flex-direction: column;
-  background: rgba(255,255,255, 0.4);
+  background: rgba(255, 255, 255, 0.4);
   border-radius: 6px;
   @media (min-width: 668px) {
     width: 80%;
@@ -20,16 +20,16 @@ const Form = styled.form`
   @media (min-width: 992px) {
     width: 50%;
   }
-`
+`;
 const Label = styled.label`
   width: 100%;
   padding: 5px 0;
-`
+`;
 const LabelText = styled.p`
   margin: 15px 0 8px 0;
   font-size: 16px;
   font-weight: bold;
-`
+`;
 const Input = styled.input`
   width: 100%;
   padding: 10px;
@@ -37,44 +37,44 @@ const Input = styled.input`
   border-radius: 3px;
   font-size: 16px;
   font-family: 'Open Sans', sans-serif;
-`
+`;
 
 export const RegistryForm = () => {
-
   const [formValues, setFormValues] = useState({
-    name: "",
-    email: "",
-    password: "",
+    name: '',
+    email: '',
+    password: ''
   });
 
-  const clearInputs = () => {
-    setFormValues({
-      name: "",
-      email: "",
-      password: "",
-    })
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
+  const handleSubmit = event => {
+    event.preventDefault();
     // POST formValues to API
-    fetch("http://localhost:8080/users", {
-      method: "POST", body: JSON.stringify(formValues), headers: { "Content-Type": "application/json" }
+
+    fetch('http://localhost:8080/users', {
+      method: 'POST',
+      body: JSON.stringify(formValues),
+      headers: { 'Content-Type': 'application/json' }
     }).then(() => {
       // You'll need to store the access token you get back in the browser using local storage
       // and then use that token when making other requests to your API
-      console.table(formValues)
-      window.alert(`Registry done for ${formValues.name}.`)
-      clearInputs()
-    })
-  }
+      console.table(formValues);
+      window.alert(`Registry done for ${formValues.name}.`);
+      setFormValues({
+        name: '',
+        email: '',
+        password: ''
+      });
+    });
+  };
 
   return (
     <Form onSubmit={handleSubmit}>
       <Label>
         <LabelText>Name</LabelText>
         <Input
-          onChange={event => setFormValues({ ...formValues, name: event.target.value })}
+          onChange={event =>
+            setFormValues({ ...formValues, name: event.target.value })
+          }
           value={formValues.name}
           type="text"
           placeholder="Your name"
@@ -87,7 +87,9 @@ export const RegistryForm = () => {
       <Label>
         <LabelText>E-mail</LabelText>
         <Input
-          onChange={event => setFormValues({ ...formValues, email: event.target.value })}
+          onChange={event =>
+            setFormValues({ ...formValues, email: event.target.value })
+          }
           value={formValues.email}
           type="email"
           placeholder="mail@mail.com"
@@ -100,7 +102,9 @@ export const RegistryForm = () => {
       <Label>
         <LabelText>Choose password</LabelText>
         <Input
-          onChange={event => setFormValues({ ...formValues, password: event.target.value })}
+          onChange={event =>
+            setFormValues({ ...formValues, password: event.target.value })
+          }
           value={formValues.password}
           type="password"
           placeholder="**********"
@@ -109,8 +113,7 @@ export const RegistryForm = () => {
           required
         />
       </Label>
-      <Button type="submit" title='Register' />
-
+      <Button type="submit" title="Register" />
     </Form>
-  )
-}
+  );
+};
